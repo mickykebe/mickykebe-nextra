3ed21241bffdbf48e0d66fe3dfd64f1c486a78ba
Michael Tedla <mickykebe@gmail.com>
2023-09-04 10:34:24 -0500

    fix portfolio image sizing and start hulusira

--- components/PortfolioItem.jsx
+++ components/PortfolioItem.jsx
@@ -9,19 +9,17 @@ export default function PortfolioItem({
   screenshotImageSrcList,
   description,
   href,
-  width,
-  height,
+  visualClassName,
 }) {
   return (
-    <div className="mb-6 grid gap-x-6 gap-y-4">
+    <div className="mb-6 flex flex-col gap-4">
       <Link href={href} className="text-2xl font-bold no-underline">
         {title}
       </Link>
       <PortfolioVisual
         caption={screenshotCaption}
         imageSrcList={screenshotImageSrcList}
-        width={width}
-        height={height}
+        className={visualClassName}
       />
       <p
         className={`relative mb-1 h-32 overflow-hidden text-sm leading-relaxed after:absolute after:bottom-0 after:left-0 after:right-0 after:top-0 after:block after:bg-gradient-to-b after:from-transparent after:to-white after:content-[''] dark:after:to-slate-900`}>
--- components/PortfolioVisual.jsx
+++ components/PortfolioVisual.jsx
@@ -1,23 +1,22 @@
 import * as React from "react";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
 
-export default function PortfolioVisual({
-  caption,
-  imageSrcList,
-  width,
-  height,
-}) {
+export default function PortfolioVisual({ caption, imageSrcList, className }) {
   return (
     <div className="flex flex-col space-y-1 rounded-md bg-zinc-300 p-3 dark:bg-slate-800">
       <div className="flex space-x-4 overflow-x-scroll rounded-sm bg-zinc-200 p-4 dark:bg-slate-700">
         {imageSrcList.map((imageSrc, index) => (
-          <Image
-            key={index}
-            src={imageSrc}
-            className="m-0 rounded-sm shadow-md"
-            width={width}
-            height={height}
-          />
+          <figure
+            className={twMerge(clsx("relative m-0 flex-shrink-0", className))}>
+            <Image
+              key={index}
+              src={imageSrc}
+              fill
+              className="m-0 rounded-sm shadow-md"
+            />
+          </figure>
         ))}
       </div>
       <div className="flex items-center">
